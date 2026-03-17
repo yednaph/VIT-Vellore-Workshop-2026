7523213614176d210569c9d7387b1bac4b015ca5
Himanshu Pandey <133879303+yednaph@users.noreply.github.com>
2026-03-17 08:22:27 +0530

    Delete phonon directory

--- phonon/autorun.sh
+++
@@ -1,5 +0,0 @@
-mpirun -np 8 pw.x <scf.in> scf.out
-mpirun -np 8 ph.x -i ph.in > ph.out
-mpirun -np 8 q2r.x -i q2r.in > q2r.out
-mpirun -np 8 matdyn.x <matdyn.in> matdyn.out
-plotband.x <plotband.in> plotband.out
